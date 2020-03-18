8e2da696687e79d9772ffd48121c350c2ca2586c
Jamie <j891319@protonmail.com>
2020-03-18 10:59:35 +0800

    optionChoices.py works better, but still buggy

--- optionChoices.py
+++ optionChoices.py
@@ -5,72 +5,85 @@ chemistry = []
 history = []
 geography = []
 computerScience = []
+choice2 = ''
+choice1 = ''
+name = ''
+completed=0
+
 
 def addOneStudent():
     print("one student has been assigned to their two choices of subject")
-    print("This will now loop to start for the next student")
+    print("This will now loop to start for the next student \n current numbers are:")
+    print("biology:",len(biology))
+    print("chemistry:",len(chemistry))
+    print("history:",len(history))
+    print("georgraphy:",len(geography))
+    print("computer science:",len(computerScience))
 
-def addName2():
-    if choice2 == subject[0] or subject[1] or subject[2] or subject[3] or subject[4]:
-            if choice2 == subject[0]:
-                    biology.append(name)
-                    addOneStudent()
-            elif choice2 == subject[1]:
-                    chemistry.append(name)
-                    addOneStudent()
-            elif choice2 == subject[2]:
-                    history.append(name)
-                    addOneStudent()
-            elif choice2 == subject[3]:
-                    geography.append(name)
-                    addOneStudent()
-            elif choice2 == subject[4]:
-                    computerScience.append(name)
-                    addOneStudent()
+
 
 def choicePick2():
-    While True:
+    while True:
         choice2 = input("what is your second choice?")
-        addName2()
+        if choice2 == subject[0]:
+                biology.append(name)
+                addOneStudent()
+        elif choice2 == subject[1]:
+                chemistry.append(name)
+                addOneStudent()
+        elif choice2 == subject[2]:
+                history.append(name)
+                addOneStudent()
+        elif choice2 == subject[3]:
+                geography.append(name)
+                addOneStudent()
+        elif choice2 == subject[4]:
+                computerScience.append(name)
+                addOneStudent()
+        else:
+            print("invalid second choice, case sensitive, please retry. only these are allowed", subject)
 
 
 def addName1():
-    if choice1 == subject[0] or subject[1] or subject[2] or subject[3] or subject[4]:
-            if choice1 == subject[0]:
-                    biology.append(name)
-                    choicePick2()
-            elif choice1 == subject[1]:
-                    chemistry.append(name)
-                    choicePick2()
-            elif choice1 == subject[2]:
-                    history.append(name)
-                    choicePick2()
-            elif choice1 == subject[3]:
-                    geography.append(name)
-                    choicePick2()
-            elif choice1 == subject[4]:
-                    computerScience.append(name)
-                    choicePick2()
+    while True:
+        choice1 = input("what is your first choice?")
+        if choice1 == subject[0]:
+                biology.append(name)
+                choicePick2()
+        elif choice1 == subject[1]:
+                chemistry.append(name)
+                choicePick2()
+        elif choice1 == subject[2]:
+                history.append(name)
+                choicePick2()
+        elif choice1 == subject[3]:
+                geography.append(name)
+                choicePick2()
+        elif choice1 == subject[4]:
+                computerScience.append(name)
+                choicePick2()
+        else:
+            print("please choose from the five allowed subject", subject)
 
 
 
 def start():
+    for x in subject:
+        print(x)
+    print("here are the subjects you can choose from. These are case sensitive")
     while True:
-    name = input("what is your name?")
-    if name.isalpha():
-        while True:
-            choice1 = input("what is your first choice?")
-            addName()
-            else:
-                print("please try again")         
+        name = input("what is your name?")
+        if name.isalpha():
+                addName1()
+        else:
+            print("please try again, no numbers in names")         
     else:
         print("invalid input, no numbers in name")
 
-for x in range(1,40):
-    start()
+start()
 
 print(len(biology))
 print(len(chemistry))
 print(len(history))
-print(len(Geography))
+print(len(geography))
 print(len(computerScience))
